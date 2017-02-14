69688cb310b174d3c919002bc368145d629276ca
RossDwyer <ross.dwyer@uq.edu.au>
2017-02-14 20:37:15 +1000

    Added install packages and project info

---
+++ R code/step 1_packages to install.R
@@ -0,0 +1,20 @@
+## First run this to install the required packages for this Animal Movement R workshop.
+
+install.packages(c("sp",
+                   "rgdal",
+                   "rgeos",
+                   "adehabitatLT",
+                   "adehabitatHR".
+                   "leaflet",
+                   "htmlwidgets",
+                   "RColorBrewer",
+                   "knitr",
+                   "spacetime",
+                   "plotKML",
+                   "tidyverse",
+                   "circular"), 
+                 repos='http://cran.us.r-project.org')
+
+
+
+                 
